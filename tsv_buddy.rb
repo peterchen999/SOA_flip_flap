9e41268e6f717fc2c929589735ea551208034dc0
peterchen999 <peter.chen999@gmail.com>
2022-10-05 22:04:59 +0800

    add - finished tsv_buddy

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,45 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    lines = tsv.split(/\n/)
+    rows = lines.map { |line| line.split("\t") }
+    keys = lines.first.split("\t")
+    # puts "keys are #{keys}"
+    @data = rows[1..].map do |line|
+      keys.zip(line).to_h
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    file_str = generate_title_str(data.first.keys, '')
+    generate_data_str(data, file_str)
   end
+end
+
+def generate_title_str(titles, file_str)
+  titles.each do |val|
+    file_str << val
+    file_str << "\t"
+  end
+  file_str = file_str.chop
+  file_str << "\n"
+  file_str
+end
+
+def generate_data_str(yml_array, file_str)
+  yml_array.each do |val|
+    val.each do |_key, value|
+      file_str << value
+      file_str << "\t"
+    end
+    file_str = file_str.chop
+    file_str << "\n"
+  end
+  file_str
 end
